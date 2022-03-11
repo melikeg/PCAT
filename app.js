7bba21a931bb03908bffe8fcf9f10453b6a04574
Melike Görgülü <melikegorgulu.119@gmail.com>
2022-03-12 01:00:24 +0300

    image upload process

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 
+const fs = require('fs');
 const path = require('path');
 const ejs = require('ejs');
 const Photo = require('./models/Photos');
@@ -20,10 +22,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true })); //url'deki datayı okumamızı sağlar
 app.use(express.json()); //url'deki datayı json formatına dönüştürmeyi sağlar
+app.use(fileUpload());
 
 //ROUTES
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({});
+  const photos = await Photo.find({}).sort('-dateCreated');
   res.render('index', {
     photos,
   });
@@ -48,9 +51,28 @@ app.get('/add', (req, res) => {
 
 //add.ejs dosyası içinde form etiketinde action="/photos" kısmında yakalıyor
 app.post('/photos', async (req, res) => {
+  //console.log(req.files.image);
   //forma girilen veriler gönderildiğinde yeni photo objesi yaratıyor
-  await Photo.create(req.body);
-  res.redirect('/'); //req-res döngüsünün bitmesi için anasayfaya yönlendirme yapıyoruz
+  //await Photo.create(req.body);
+  //res.redirect('/'); //req-res döngüsünün bitmesi için anasayfaya yönlendirme yapıyoruz
+
+  const uploadDir = 'public/uploads';
+  //existsSync dosyanın olup olm. kontrol eder
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  //__dirname var olan klasörün kendisini
+  const uploadImage = req.files.image;
+  const uploadPath = __dirname + '/public/uploads/' + uploadImage.name;
+
+  uploadImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadImage.name,
+    });
+    res.redirect('/');
+  });
 });
 
 const port = 3000;
